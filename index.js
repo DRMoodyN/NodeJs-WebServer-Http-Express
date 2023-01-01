e0b3d1ef78cc77b45026167e213a8636f00957df
DRMoodyN <drmoodyn@gmail.com>
2022-12-31 21:46:28 -0600

    Sirviendo algo mas real

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.use("*", (req, res) => {
+    res.sendFile(__dirname + "/public/404.html");
+});
+
 app.listen(5000, () => {
     console.log(`Servidor http://localhost:5000`);
 });

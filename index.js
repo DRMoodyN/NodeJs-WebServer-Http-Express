9b2b556e80a47bc608b34bb90c5cbfc845745f31
DRMoodyN <drmoodyn@gmail.com>
2022-12-31 22:29:16 -0600

    Mejornado rutas

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.use("/generic", (req, res) => {
+    res.sendFile(__dirname + "/public/generic.html");
+});
+
+app.use("/elements", (req, res) => {
+    res.sendFile(__dirname + "/public/elements.html");
+});
+
 app.use("*", (req, res) => {
     res.sendFile(__dirname + "/public/404.html");
 });
